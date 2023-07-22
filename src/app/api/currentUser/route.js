be1f61ca3c97426dd2edbcdef5c559405d506eaf
Ivo Spironello <99614711+aphxtwin@users.noreply.github.com>
2023-07-22 20:00:28 -0300

    ahora no se pierde la sesion2

--- src/app/api/currentUser/route.js
+++ src/app/api/currentUser/route.js
@@ -6,7 +6,7 @@ const secretKey = process.env.JWT_SECRET_KEY;
 // eslint-disable-next-line import/prefer-default-export
 export async function GET(req) {
   // get the JWT token from the cookie
-  const token = req.cookies.get('user-token').value;
+  const token = req.cookies.get('user-token');
   if (!token) {
     return new NextResponse(JSON.stringify('No user logged in'), {
       status: 401,
@@ -15,7 +15,7 @@ export async function GET(req) {
   }
 
   // decode the JWT token to get the user data
-  const user = jwt.verify(token, secretKey);
+  const user = jwt.verify(token.value, secretKey);
   // return the user data
   return new NextResponse(JSON.stringify(user), {
     status: 200,
