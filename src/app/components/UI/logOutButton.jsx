93a6ff3f33116989099162c2c7018b8fd49106ef
Ivo Spironello <99614711+aphxtwin@users.noreply.github.com>
2023-07-24 11:54:07 -0300

    log out2

--- src/app/components/UI/logOutButton.jsx
+++ src/app/components/UI/logOutButton.jsx
@@ -1,14 +1,18 @@
 'use client';
 
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { Button } from '@mui/material';
 import { useRouter } from 'next/navigation';
+import { logOut } from '../../redux/slices/authSlice';
 
 function LogOutButton() {
+  const dispatch = useDispatch();
   const router = useRouter();
   const handleLogout = async () => {
     const res = await fetch('/api/auth/logOut', { method: 'POST' });
     if (res.ok) {
+      dispatch(logOut());
       router.push('/');
     }
   };
